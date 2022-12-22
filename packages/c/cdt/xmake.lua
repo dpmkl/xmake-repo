04d1f47293df8dd218e2fc301a7af4ffcc06fa62
Michael Craggs <michael.craggs@swarm-analytics.com>
2022-12-22 19:55:11 +0100

    Builds

--- packages/c/cdt/xmake.lua
+++ packages/c/cdt/xmake.lua
@@ -8,19 +8,11 @@ package("cdt")
 
     on_install(function (package)
         local configs = {}
-        io.writefile("xmake.lua", [[
-            add_rules("mode.release", "mode.debug")
-            target("cdt")
-                set_kind("$(kind)")
-                add_files("src/*.c")
-                add_headerfiles("src/(*.h)")
-        ]])
         if package:config("shared") then
             configs.kind = "shared"
-        end
-        import("package.tools.xmake").install(package, configs)
+        end	
+	table.insert(configs, "-DCDT_USE_AS_COMPILED_LIBRARY=ON")
+	os.cd("CDT")
+	import("package.tools.cmake").install(package, configs)
     end)
 
-    on_test(function (package)
-        assert(package:has_cfuncs("foo", {includes = "foo.h"}))
-    end)
